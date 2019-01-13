43096eccade5987b0d9e2bdad5424513687972ed
aviv <aviv1620@gmail.com>
2019-01-13 13:34:59 +0200

    example

---
+++ src/ex4_example/Stam.java
@@ -0,0 +1,12 @@
+package ex4_example;
+
+public class Stam {
+
+	public static void main(String[] args) {
+		String s = "Ex4_OOP_example8";
+		double d = s.hashCode();
+		System.out.println(d);
+
+	}
+
+}
